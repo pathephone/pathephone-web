bc2b21e6629a46288da8fc5d06580cded8b85fbe
Max Ilyin <negamaxi@gmail.com>
2018-11-17 11:54:11 +0300

    cleanup + .env

--- src/components/App/AppLoadingScreen.jsx
+++
@@ -1,9 +0,0 @@
-// @flow strict
-
-import * as React from 'react'
-
-import styles from 'styles/App.module.css'
-
-export const AppLoadingScreen = () => (
-  <div className={styles.AppLoadingScreen} />
-)
--- src/types/albums.js
+++
@@ -1,5 +0,0 @@
-// @flow strict
-
-export type TAlbum = {
-  
-}
