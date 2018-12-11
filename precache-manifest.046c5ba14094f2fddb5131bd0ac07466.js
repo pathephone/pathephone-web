b58386c16e4b5593fd927048a1e4c2993076bcaa
Maxim Ilyin <negamaxi@gmail.com>
2018-12-11 22:34:59 +0300

    Updates

---
+++ precache-manifest.046c5ba14094f2fddb5131bd0ac07466.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pathephone-web/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "83e9ffe3a1e7775ea34b",
+    "url": "/pathephone-web/static/js/runtime~main.83e9ffe3.js"
+  },
+  {
+    "revision": "a6f926ab0f89138a0087",
+    "url": "/pathephone-web/static/js/main.a6f926ab.chunk.js"
+  },
+  {
+    "revision": "1a2fe23bbee4e72efb16",
+    "url": "/pathephone-web/static/js/1.1a2fe23b.chunk.js"
+  },
+  {
+    "revision": "a6f926ab0f89138a0087",
+    "url": "/pathephone-web/static/css/main.bb9ee306.chunk.css"
+  },
+  {
+    "revision": "739bc89e2ae048b0d7c10b2ff9099c19",
+    "url": "/pathephone-web/index.html"
+  }
+];
