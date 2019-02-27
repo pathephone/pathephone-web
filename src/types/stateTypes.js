36fb1d625d66f661970a9b3b66b5d5297194826d
Maxim Ilyin <negamaxi@gmail.com>
2019-02-27 22:05:11 +0300

    back to string for playlist track type

--- src/types/stateTypes.js
+++ src/types/stateTypes.js
@@ -1,33 +1,33 @@
 // @flow strict
 
 export type TPlaylistTrack = {|
-  id: number,
-    title: string,
-      artistName: string,
-        audioSrc: string
-          |};
+  id: string,
+  title: string,
+  artistName: string,
+  audioSrc: string
+|};
 
 export type TFeedAlbum = {|
   id: string,
-    title: string,
-      artistName: string,
-        coverSrc: string
-          |};
+  title: string,
+  artistName: string,
+  coverSrc: string
+|};
 
 export type TFormArtist = {|
   key: string,
-    name: string
-      |};
+  name: string
+|};
 
 export type TFormTrack = {|
   key: string,
-    title: string,
-      artists: TFormArtist[],
-        audio: File
-          |};
+  title: string,
+  artists: TFormArtist[],
+  audio: File
+|};
 
 export type TFormAlbum = {|
   title: string,
-    cover: File | null,
-      tracklist: TFormTrack[]
-        |};
+  cover: File | null,
+  tracklist: TFormTrack[]
+|};
